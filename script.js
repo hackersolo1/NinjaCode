4352217d73f316110e989b9f70f101cf75bc0c3a
HackerSolo1 <eujogominecraftpaulo99@gmail.com>
2026-05-17 21:32:55 -0300

    feat: remover redirecionamento para a página inicial ao fazer logout

--- script.js
+++ script.js
@@ -349,10 +349,6 @@ document.addEventListener("DOMContentLoaded", () => {
             addNewCodeBtn.style.display = 'none';
             logoutBtn.style.display = 'none';
             avatarImg.src = "https://ui-avatars.com/api/?name=Guest&background=random&size=128";
-
-            // Volta pra home se estava em outra rota
-            if (location.hash) navigate('');
-            else showView(homeView);
             emailU = 'Desconhecido(a)';
             userName = 'Desconecido(a)';
         }
