e0ede7b3b1a28f2a4f4d1298b178801393f0113b
HackerSolo1 <eujogominecraftpaulo99@gmail.com>
2026-05-17 21:12:24 -0300

    feat: redirecionar usuário para a página inicial após logout

--- script.js
+++ script.js
@@ -349,6 +349,7 @@ document.addEventListener("DOMContentLoaded", () => {
             addNewCodeBtn.style.display = 'none';
             logoutBtn.style.display = 'none';
             avatarImg.src = "https://ui-avatars.com/api/?name=Guest&background=random&size=128";
+            window.location.href = 'home.html';
 
             // Volta pra home se estava em outra rota
             if (location.hash) navigate('');
